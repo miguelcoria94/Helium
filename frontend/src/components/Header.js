9965d02852ae38f82afe97f06d45b74abe146678
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 14:57:06 -0600

    addded padding to icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/cart"><i className="fas fa-shopping-cart p-1"></i>Cart</Nav.Link>
               <Nav.Link href="/login">Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
