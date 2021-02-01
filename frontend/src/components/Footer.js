fe9c9fadacc84b44666e6d3b64646cab9b01ceaa
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 14:40:39 -0600

    fix footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap/";
 
 const Footer = () => {
     return (
         <footer>
-            Footer
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        Copyright &copy; Beauty Empire
+                    </Col>
+                </Row>
+
+            </Container>
         </footer>
     )
 }
