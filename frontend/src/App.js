a2bfd213eafa699d6f409675dcea0e8be9b7c7be
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 13:24:19 -0600

    set up app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Beauty Empire !</h1>
+      <Header />
+      <main>
+        <h1>Beauty Empire !</h1>
+      </main>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            header
+        </header>
     )
 }
 
