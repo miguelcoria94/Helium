9e846c8ab614014fa4cddcceacb2bd3db380f1e5
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 14:52:30 -0600

    setup login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,12 +3,19 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
-        <header>
-            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-                <Navbar.Brand href="/">Beauty Empire</Navbar.Brand>
-            </Navbar>
-        </header>
-    )
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Navbar.Brand href="/">Beauty Empire</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+    );
 }
 
 export default Header
