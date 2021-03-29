87998c8ad3e73a82f58c3e82cb1861bc39cc15b9
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-29 17:07:14 -0500

    added product title

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,12 +3,22 @@ import {Card } from 'react-bootstrap'
 
 const Product = ({product}) => {
     return (
-        <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant='top' />
-            </a>
-        </Card>
-    )
+      <Card className="my-3 p-3 rounded">
+        <a href={`/product/${product._id}`}>
+          <Card.Img src={product.image} variant="top" />
+        </a>
+
+        <Card.Body>
+          <a href={`/product/${product._id}`}>
+            <Card.Title>
+                        <strong>
+                            {product.name}
+                </strong>
+            </Card.Title>
+          </a>
+        </Card.Body>
+      </Card>
+    );
 }
 
 export default Product;
