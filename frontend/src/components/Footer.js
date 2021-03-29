d5646a76d3c18ec658c206afb156dd8e588852a4
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-29 16:21:59 -0500

    working on fottoter

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,17 +3,21 @@ import { Container, Row, Col } from "react-bootstrap/";
 
 const Footer = () => {
     return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">
-                        Copyright &copy; Beauty Empire
-                    </Col>
-                </Row>
-
-            </Container>
-        </footer>
-    )
+      <footer>
+        <Container>
+          <Row>
+            <Col className="text-center py-3">LinkedIn</Col>
+            <Col className="text-center py-3">Github</Col>
+            <Col className="text-center py-3">Other Projects</Col>
+          </Row>
+          <Row>
+            <Col className="text-center py-3">
+              Copyright &copy; Beauty Empire
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    );
 }
 
 export default Footer
